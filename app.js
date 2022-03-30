68393bab4860868fbf2940f84558719a91628c97
Khaled Abo <khaledabo@Khaleds-MacBook-Pro.local>
2022-03-30 08:39:28 +0200

    adding hash password

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const session = require("express-session");
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
+
+const { authenticate } = require("./db");
+const User = require("./db/models/User");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -8,8 +13,35 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8000;
 
-const handleRegister = (req, res, next) => {
+const handleRegister = async (req, res, next) => {
   // Handle register user
+  console.log(req.body);
+
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  console.log(hash);
+  try {
+    await User.create({ email: req.body.email, password: hash });
+  } catch (error) {
+    console.log(error);
+  }
+  next();
+};
+
+const validate = async (req, res, next) => {
+  let user = null;
+  console.log(req.body);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  try {
+    user = await User.findOne({ where: { email: req.body.email } });
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+  if (user && user.password === hash) {
+    req.body.status = "success";
+  } else {
+    req.body.status = "error";
+  }
 
   next();
 };
@@ -21,8 +53,20 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 app.post("/register", handleRegister, (req, res) => {
-  console.log(req.body);
   res.redirect("/");
 });
-
+app.get("/login", (reg, res) => {
+  res.render("login");
+});
+app.post("/login", validate, (req, res) => {
+  if (req.body.status == "success") {
+    res.redirect("/welcome");
+  } else {
+    res.redirect("/");
+  }
+});
+app.get("/welcome", (req, res) => {
+  res.render("welcome");
+});
+authenticate();
 app.listen(PORT, () => console.log("running on port" + PORT));
--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
 const db = require("./db");
+const User = require("./models/User");
 
 async function authenticate() {
   try {
     db.authenticate();
+    User.sync();
     console.log("Success Authentication");
   } catch (error) {
     console.log("error when creating database : " + error);
   }
 }
 
-authenticate();
-
-module.exports = authenticate;
+module.exports = { authenticate };
